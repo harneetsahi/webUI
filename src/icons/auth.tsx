e054be352d7903848121904bd61c87ffc7af119f
Harneet <harneetsahi8@gmail.com>
2025-07-28 20:21:44 -0500

    update:  camelCase to replace kebab case

--- src/icons/auth.tsx
+++ src/icons/auth.tsx
@@ -6,11 +6,11 @@ export const Auth = () => {
       fill="none"
       width={50}
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
--- src/icons/telegram.tsx
+++ src/icons/telegram.tsx
@@ -6,11 +6,11 @@ export const Telegram = () => {
       xmlns="http://www.w3.org/2000/svg"
       width={60}
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
